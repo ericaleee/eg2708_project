586536070f888092a2a1d7970ab4b34d03c9d05b
erica <193383s@mymail.nyp.edu.sg>
2021-01-26 23:16:38 +0800

    Create profile.dart

---
+++ ericalee_project/lib/profile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class Profile extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.indigo[50],
+      );
+    
+  }
+}
