ffb022b89d2aa375bf365c63be08ed141b201478
erica <193383s@mymail.nyp.edu.sg>
2021-02-16 03:57:41 +0800

    Create user_list.dart

---
+++ ericalee_project/lib/user_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'users.dart';
+import 'user_tile.dart';
+
+class UserList extends StatefulWidget {
+  @override 
+  _UserListState createState() => _UserListState();
+}
+
+class _UserListState extends State<UserList> {
+  @override 
+  Widget build(BuildContext context){
+    final users = Provider.of<List<User>>(context) ?? [];
+    
+    
+    return ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (context, index){
+        return UserTile(
+          user: users[index]
+        );
+      },
+
+    );
+
+  }
+}
