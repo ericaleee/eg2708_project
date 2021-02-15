a7c098801d410c44995807652bd9f43dddfcf66f
erica <193383s@mymail.nyp.edu.sg>
2021-02-15 18:12:57 +0800

    Update FirstScreen.dart

--- ericalee_project/lib/FirstScreen.dart
+++ ericalee_project/lib/FirstScreen.dart
@@ -1,7 +1,10 @@
+import 'package:ericalee_project/ThirdScreen.dart';
 import 'package:flutter/material.dart';
 import 'about.dart';
 import 'main.dart';
 import 'drawer.dart';
+import 'signin.dart';
+import 'home.dart';
 
 class FirstScreen extends StatelessWidget{
   @override 
@@ -12,14 +15,16 @@ class FirstScreen extends StatelessWidget{
           children: <Widget> [
              Padding(
                 padding: const EdgeInsets.all(20.0),       
-                child: Row(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    Image.asset('images/flag_down_fares.jpg',),
                     RaisedButton(
                       child: Text('Back'),
                       color: Colors.indigo[100],
                       onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>Third()));
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>Home())); 
+                        
                       },
                     ),
                   ],
