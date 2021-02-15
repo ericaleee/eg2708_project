bea10bb53989aae91bee9e950f403946f65225d3
erica <193383s@mymail.nyp.edu.sg>
2021-02-16 03:57:10 +0800

    Update profile.dart

--- ericalee_project/lib/profile.dart
+++ ericalee_project/lib/profile.dart
@@ -1,77 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ericalee_project/auth.dart';
+import 'package:ericalee_project/settings_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'database.dart';
+import 'package:provider/provider.dart';
+import 'user_list.dart';
+import 'user.dart';
+import 'settings_form.dart';
+
 
 
 class Profile extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-            height: 240,
-            child: Card(
-                color: Colors.indigo[100],
-                child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Container(
-                                height: 72.0,
-                                width: 72.0,
-                                decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    image: DecorationImage(
-                                      image: AssetImage('images/jungkook.jpeg'),
-                                      fit: BoxFit.contain,
-                                    )),
-                              ),
-                            ),
-                            Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '   Erica Lee',
-                                  style: Theme.of(context).textTheme.headline5,
-                                ),
-                                Text(
-                                  '   Experienced App Developer',
-                                  style: Theme.of(context).textTheme.headline6,
-                                )
-                              ],
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 28),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '180 Ang Mo Kio Ave 8',
-                              style: Theme.of(context).textTheme.subtitle1,
-                            ),
-                            Text(
-                              '(65) 9211 1234',
-                              style: Theme.of(context).textTheme.subtitle1,
-                            )
-                          ],
-                        ),
-                        SizedBox(height: 5),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Icon(Icons.accessibility),
-                            Icon(Icons.timer),
-                            Icon(Icons.phone_android),
-                            Icon(Icons.phone_iphone),
-                          ],
-                        ),
-                      ],
-                    )))));
+
+    void _showSettingsPanel() {
+      showModalBottomSheet(context: context, builder: (context){
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+          child: SettingsForm(),
+        );
+      });
+    }
+
+   return StreamProvider<List<User>>.value(
+     value: DatabaseService().users,
+     child: Scaffold(
+       backgroundColor: Colors.indigo[50],
+          appBar: AppBar(
+            backgroundColor: Colors.indigo[100],
+          actions: <Widget>[
+             FlatButton.icon(
+               icon: Icon(Icons.person),
+               label: Text('settings'),
+               onPressed: () => _showSettingsPanel(),
+             )]
+            ),
+       
+     
+      body: UserList(),
+     
+   ));
   }
 }
