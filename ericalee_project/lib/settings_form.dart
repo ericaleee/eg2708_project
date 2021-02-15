33084ae736d5ea0226d8288f60e35bd0bce72f55
erica <193383s@mymail.nyp.edu.sg>
2021-02-16 03:57:20 +0800

    Create settings_form.dart

---
+++ ericalee_project/lib/settings_form.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'database.dart';
+import 'user.dart';
+import 'package:provider/provider.dart';
+import 'loading.dart';
+
+class SettingsForm extends StatefulWidget {
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+
+  final _formKey = GlobalKey<FormState>();
+  
+  String _currentName;
+  String _currentEmail;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final user = Provider.of<User>(context);
+
+    return StreamBuilder<UserData>(
+    stream: DatabaseService(uid: user.uid).userData,
+    builder: (context, snapshot){
+      if(snapshot.hasData){
+
+      UserData userData = snapshot.data;
+
+      return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Update your profile',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            initialValue: userData.name,
+            decoration: textInputDecoration,
+            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+            onChanged: (val) => setState(() => _currentName = val),
+          ),
+          SizedBox(height: 20.0,),
+          TextFormField(
+            initialValue: userData.email,
+            decoration: textInputDecoration,
+            validator: (val) => val.isEmpty ? 'Please enter an email' : null,
+            onChanged: (val) => setState(() => _currentEmail = val),
+          ),
+          RaisedButton(
+            color: Colors.pink[400],
+            child:Text(
+              'Update',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () async {
+              if(_formKey.currentState.validate()){
+                await DatabaseService(uid: user.uid).updateUserData(
+                  _currentName ?? userData.name,
+                  _currentEmail ?? userData.email
+                );
+                Navigator.pop(context);
+              }
+            },
+          )
+        ],
+      ));
+      } else{
+          return Loading();
+      }
+    
+    });
+  }
+}
