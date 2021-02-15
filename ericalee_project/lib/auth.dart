ad864fef102595ee862d5550e7c2f79765ddea78
erica <193383s@mymail.nyp.edu.sg>
2021-02-15 18:15:05 +0800

    Create auth.dart

---
+++ ericalee_project/lib/auth.dart
@@ -0,0 +1,73 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'user.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user obj based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user){
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<User> get user{
+    return _auth.onAuthStateChanged
+//.map((FirebaseUser user) => _userFromFirebaseUser(user));
+      .map(_userFromFirebaseUser);
+  }
+
+  // sign in anon
+  Future signInAnon() async {
+    try{
+     // ignore: omit_local_variable_types
+     AuthResult result = await _auth.signInAnonymously();
+     // ignore: omit_local_variable_types
+     FirebaseUser user = result.user;
+     return _userFromFirebaseUser(user);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign in with email and password
+  Future signInWithEmailandPassword( String email, String password)
+  async{
+    try{
+      // ignore: omit_local_variable_types
+      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      // ignore: omit_local_variable_types
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+
+  // register with email and password
+  Future registerWithEmailandPassword( String email, String password)
+  async{
+    try{
+      // ignore: omit_local_variable_types
+      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      // ignore: omit_local_variable_types
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign out
+  Future signOut() async {
+    try{
+      return await _auth.signOut();
+    } catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+}
