9793d6732851531433ccb8f12a65e344cfa0d40c
erica <193383s@mymail.nyp.edu.sg>
2021-02-15 18:16:14 +0800

    Update SecondScreen.dart

--- ericalee_project/lib/SecondScreen.dart
+++ ericalee_project/lib/SecondScreen.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'locations.dart' as locations;
 
-class MyData {
-  final String firstName;
-  final String lastName;
-  MyData(this.firstName, this.lastName);
+void main() {
+  runApp(SecondScreen());
 }
 
-class SecondScreen extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return Container(
-      child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-            new Text(
-              'Second Screen',
-            ),
-            Icon(Icons.directions_transit),
-          ],
+class SecondScreen extends StatefulWidget {
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+  }
+
+
+class _SecondScreenState extends State<SecondScreen> {
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: const LatLng(1.290270, 103.851959),
+            zoom: 2,
+          ),
+          markers: _markers.values.toSet(),
+        ),
       ),
     );
   }
