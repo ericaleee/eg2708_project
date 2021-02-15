000eb91a2b7bfe65dba075ce5ccfa1a708148524
erica <193383s@mymail.nyp.edu.sg>
2021-02-15 18:16:24 +0800

    Update ThirdScreen.dart

--- ericalee_project/lib/ThirdScreen.dart
+++ ericalee_project/lib/ThirdScreen.dart
@@ -1,33 +1,43 @@
 import 'package:flutter/material.dart';
 
-class ThirdScreen extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return Container(
-      child: ListView(
-        children: <Widget> [
-          ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Map'),
-            onTap: (){
-              print('Map');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.photo_album),
-            title: Text('Album'),
-            onTap: (){
-              print('Album');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.phone),
-            title: Text('Phone'),
-            onTap: (){
-              print('Phone');
-            },
-          ),
-        ],)
+
+class ThirdScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+       
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Text('Singapore', style: new TextStyle(color: Colors.white)),
+                    Text('Sunny', style: new TextStyle(color: Colors.white, fontSize: 32.0)),
+                    Text('32°C',  style: new TextStyle(color: Colors.white)),
+                    Image.network('https://openweathermap.org/img/w/01d.png'),
+                    Text('Feb 9, 2021', style: new TextStyle(color: Colors.white)),
+                    Text('10:22', style: new TextStyle(color: Colors.white)),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: IconButton(
+                  icon: new Icon(Icons.refresh),
+                  tooltip: 'Refresh',
+                  onPressed: () => null,
+                  color: Colors.white,
+                ),
+              )
+            ]
+          )
+        )
+      ),
     );
   }
 }
