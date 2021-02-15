4affdc8e62ab489f4ccd78a4f16b73e1bf90941d
erica <193383s@mymail.nyp.edu.sg>
2021-02-16 03:57:46 +0800

    Create user_tile.dart

---
+++ ericalee_project/lib/user_tile.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'users.dart';
+
+class UserTile extends StatelessWidget {
+
+  final User user;
+  UserTile({this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+        child: ListTile(
+          leading: CircleAvatar( 
+            radius: 25.0,
+            backgroundColor: Colors.brown,
+            backgroundImage: AssetImage('images/jungkook.jpeg'),
+          ),
+          title: Text(user.name),
+          subtitle: Text(user.email),
+        ),
+
+      ),
+    );
+  }
+}
