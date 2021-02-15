67017e35d93070bb08c2a74e13dbce301e676d01
erica <193383s@mymail.nyp.edu.sg>
2021-02-16 03:57:35 +0800

    Update user.dart

--- ericalee_project/lib/user.dart
+++ ericalee_project/lib/user.dart
@@ -1,8 +1,21 @@
 class User {
 
   final String uid;
+  final String email;
+  final String name;
 
+
+  User({ this.uid, this.email, this.name});
   // ignore: sort_constructors_first
-  User({ this.uid }); 
+
   
+}
+
+class UserData{
+  final String email;
+  final String name;
+  final String uid;
+
+
+  UserData({ this.uid, this.email, this.name});
 }
